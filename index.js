0079eee0588896f213b09beab6556b86d87e9add
Ubuntu <ubuntu@ip-172-31-31-78.eu-west-2.compute.internal>
2020-09-07 13:31:07 +0000

    another update

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 var mysql = require('mysql');
+const express = require('express');
+var app = express();
+const bodyparser = require('body-parser');
+app.use(bodyparser.json());
 
 var con = mysql.createConnection({
   host: "patientdb.c2lmccmuxyuo.eu-west-2.rds.amazonaws.com",
   user: "admin",
   password: "SC1234567",
-  database: "patientdb"
+  database: "patientdb",
+  multipStatements: true
 }); 
 
-
+// GET
 con.connect(function(err) {
+  if (err) throw err;
+  con.query("SELECT * FROM patient", function (err, result, fields) {
     if (err) throw err;
-    con.query("SELECT * FROM patient", function (err, result, fields) {
-      if (err) throw err;
-      console.log(result);
-    });
-  }); 
-  
+    console.log(result);
+  });
+});
+
+//POST
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql = "INSERT INTO patient (patient_id, patient_name, patient_height, patient_physician_note, patient_physician, dob) VALUES ('2', 'Emma', 5.7, 'out of bedrest and can leave', 'Dr Harris', '1983')";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
+});
+
